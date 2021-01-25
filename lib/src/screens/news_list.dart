250adad400eaec197a232150859841f1976afbfd
Tyler Thompson <th0mps0n.ty@protonmail.com>
2021-01-24 23:04:37 -0500

    began working on the UI portion of this app, created the `news_list.dart` class
    Added an exanple to `news_list.dart` to show how we are going to render the items from the news API.  Using FutureBuilder we render only what appears on / can fit on the screen.  When the user scrolls down we call the API to get the next item(s) => NICE!

---
+++ lib/src/screens/news_list.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class NewsList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Top News'),
+      ),
+      body: buildList(),
+    );
+  }
+
+  Widget buildList() {
+    return ListView.builder(
+      itemCount: 1000,
+      itemBuilder: (context, int index) {
+        return FutureBuilder(
+          future: getFuture(),
+          builder: (context, snapshot) {
+            return Container(
+              height: 80.0,
+              child: snapshot.hasData
+                  ? Text('Im visible $index')
+                  : Text('I havent fetched data yet $index'),
+            );
+          },
+        );
+      },
+    );
+  }
+
+  getFuture() {
+    return Future.delayed(Duration(seconds: 2), () => 'hi');
+  }
+}
