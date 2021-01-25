f7f09120366b869e4f9e7dd5f3dc7f25eb72a8b2
Tyler Thompson <th0mps0n.ty@protonmail.com>
2021-01-24 23:07:53 -0500

    various updates
    Clean starting point for next lesson, removed example of FutureBuilder rendered list, removed some comments, and cleaned up a bit.

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -7,30 +7,7 @@ class NewsList extends StatelessWidget {
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: buildList(),
+      body: Text('Show a list!'),
     );
-  }
-
-  Widget buildList() {
-    return ListView.builder(
-      itemCount: 1000,
-      itemBuilder: (context, int index) {
-        return FutureBuilder(
-          future: getFuture(),
-          builder: (context, snapshot) {
-            return Container(
-              height: 80.0,
-              child: snapshot.hasData
-                  ? Text('Im visible $index')
-                  : Text('I havent fetched data yet $index'),
-            );
-          },
-        );
-      },
-    );
-  }
-
-  getFuture() {
-    return Future.delayed(Duration(seconds: 2), () => 'hi');
   }
 }
