555446371960b546417edf585f94b7c52f985880
Tyler Thompson <th0mps0n.ty@protonmail.com>
2021-01-27 07:39:00 -0500

    in progress, wiring up the bloc to  working currently building but an error state persists, this is by design so as to show how providing dart with the proper dataTypes will help this.

--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:news/src/bloc/stories_provider.dart';
 
 class NewsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final bloc = StoriesProvider.of(context);
+
+    // This is bad!!! Don't do this!
+    bloc.fetchTopIds();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Top News'),
       ),
-      body: Text('Show a list!'),
+      body: buildList(bloc),
+    );
+  }
+
+  Widget buildList(StoriesBloc bloc) {
+    return StreamBuilder(
+      stream: bloc.topIds,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        return !snapshot.data
+            ? Text('Still waiting on Ids...')
+            : ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Text(snapshot.data[index]);
+                },
+              );
+      },
     );
   }
 }
