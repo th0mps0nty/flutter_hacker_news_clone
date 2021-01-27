09f78fa940f0895297cfe3e867fd84c64b9a09c5
Tyler Thompson <th0mps0n.ty@protonmail.com>
2021-01-26 23:11:28 -0500

    forgot to add provider for topIDs stream

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:news/src/screens/news_list.dart';
+import 'bloc/stories_provider.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(context) {
-    return MaterialApp(
-      title: 'News!',
-      home: NewsList(),
+    return StoriesProvider(
+      child: MaterialApp(
+        title: 'News!',
+        home: NewsList(),
+      ),
     );
   }
 }
